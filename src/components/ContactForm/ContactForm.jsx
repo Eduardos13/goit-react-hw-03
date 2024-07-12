b37333196514e6946791ac7adac67938c3869ff7
Eduardos13 <edik.kozelsky@gmail.com>
2024-07-11 23:42:02 -0400

    partially done

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,10 +1,40 @@
 import React from "react";
 import s from "./ContactForm.module.css";
+import { Formik, Form, Field } from "formik";
 
 const ContactForm = () => {
   return (
-    <div>
-      <div className={s.wrapper}>ContactForm</div>
+    <div className={s.wrapper}>
+      <Formik
+        initialValues={{
+          name: "",
+          number: "",
+        }}
+        onSubmit={(data, action) => {
+          action.resetForm();
+          console.log(data);
+        }}
+      >
+        <Form className={s.form}>
+          <label className={s.lable}>
+            <span>Name</span>
+            <Field
+              className={s.input}
+              name="name"
+              placeholder="Contact name"
+            ></Field>
+          </label>
+          <label className={s.lable}>
+            <span>Number</span>
+            <Field
+              className={s.input}
+              name="number"
+              placeholder="Contact number"
+            ></Field>
+          </label>
+          <button type="submit">Add contact</button>
+        </Form>
+      </Formik>
     </div>
   );
 };
